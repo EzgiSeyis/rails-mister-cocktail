07e89bcaf8eafad906f78d54b5a1b763ee21df2c
Ezgi Seyis <ezgicay_1993@hotmail.com>
2019-01-24 12:40:37 +0100

    Mister Cocktail DB and Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'cocktails/index'
+  get 'cocktails/show'
+  get 'cocktails/new'
+  get 'cocktails/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: %i[index show new create]
+
+  root to: 'cocktails#index'
 end
